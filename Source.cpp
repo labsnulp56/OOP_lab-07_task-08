3bc0dfd4a84e4f5d2c94562734d3eb7aec9cf289
Vitaliy Klimov <vitalik_klimov@ukr.net>
2019-05-27 20:49:57 +0300

    final

--- Source.cpp
+++ Source.cpp
@@ -9,50 +9,45 @@ using namespace std;
 int main()
 {
 	system("chcp 1251");
-	string path = "myFile.txt";
-	string msg = "tester";
-	string out_path = "output.txt";
-	char ch;
 	vector<int> myVector(5);
+	string path = "myFile.txt";
+	string out_path = "output.txt";
 	ifstream fs;
 	ofstream output;
 	output.open(out_path);
 	fs.open(path);
-	if (!fs.is_open() && !output.is_open())
+	if (!fs.is_open())
 	{
-		cout << "Помилка відкриття файлу!" << endl;
+		cout << "Помилка відкриття файлу для зчитування!" << endl;
 	}
 	else
 	{
-		cout << "Файл успішно відкрито!" << endl;
+		cout << "Файл для зчитування успішно відкрито!" << endl;
 		for (int i = 0; i < myVector.size(); i++)
 		{
 			fs >> myVector[i];
 		}
 	}
+	!output.is_open() ? cout << "Помилка відкриття файлу для запису!" << endl : cout << "Файл для запису успішно відкрито!" << endl;
 	sort(myVector.begin(), myVector.end());
-
 	for (int i = 0; i < myVector.size(); i++)
 	{
 		cout << myVector[i] << ' ';
 	}
-
 	if (binary_search(myVector.begin(), myVector.end(), 3))
 	{
 		cout << "\n число 3 знайдено у масиві!\n";
 	}
-		
 	else 
 	{
 		cout << "\n число 3 не знайдено у масиві! \n";
 	}
-
 	for (int i = 0; i < myVector.size(); i++)
 	{
 		output << myVector[i] << " ";
 	}
-
 	fs.close();
+	output.close();
 	system("pause");
 	return 0;
 }
